27e8bd1dd90a0094961167c7449f50318e423930
a393461549 <393461549@qq.com>
2022-04-20 17:30:28 +0800

    2022.4.20

--- test.py
+++ test.py
@@ -1,24 +1,18 @@
-text2018 = open('sale2018.csv', 'r')
-text2019 = open('sale2019.csv', 'r')
-for line in text2018:
-    sale2018 = [line.strip().split(',')[0], float(line.strip().split(',')[1])]
-for line in text2019:
-    sale2019 = [line.strip().split(',')[0], float(line.strip().split(',')[1])]
-saleset2018 = {x[0] for x in sale2018}
-saleset2019 = {x[0] for x in sale2019}
-n = int(input())
-if n == 1:
-    print(sorted(list(saleset2018)))
-    print(sorted(list(saleset2019)))
-elif n == 2:
-    s2018and2019 = saleset2018 & saleset2019
-    print(sorted(list(s2018and2019)))
-elif n == 3:
-    s2018all2019 = saleset2019 | saleset2018
-    print(sorted(list(s2018all2019)))
-elif n == 4:
-    s2019 = saleset2019-saleset2018
-    print(sorted(list(s2019)))
-elif n == 5:
-    new = saleset2018 ^ saleset2019
-    print(sorted(list(new)))
+key = 'HELLOPYTHON'
+string = 'GONE WITH THE WIND'
+letter = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+set_key = set(key)
+list_letter = set(letter)
+code = list(list_letter-set_key)
+code.sort()
+lt_key=list(set_key)
+lt_key.sort(key=key.index)
+new_letter=lt_key+code
+new_string=''
+for i in string:
+    if i==' ':
+        new_string+=' '
+    else:
+        s=letter.index(i)
+        new_string+=new_letter[s]
+print (new_string)
