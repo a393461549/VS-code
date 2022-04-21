56dba29ec76196c3c134106bdae9342fb341029d
a393461549 <393461549@qq.com>
2022-04-21 17:36:57 +0800

    20220421

--- test.py
+++ test.py
@@ -1,18 +1,25 @@
-key = 'HELLOPYTHON'
-string = 'GONE WITH THE WIND'
-letter = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-set_key = set(key)
-list_letter = set(letter)
-code = list(list_letter-set_key)
-code.sort()
-lt_key=list(set_key)
-lt_key.sort(key=key.index)
-new_letter=lt_key+code
-new_string=''
-for i in string:
-    if i==' ':
-        new_string+=' '
+def menu():
+    print('''\n欢迎使用PYTHON学生通讯录
+1：添加学生
+2：删除学生
+3：修改学生信息
+4：搜索学生
+5：显示全部学生信息
+6：退出并保存''')
+dic={'张自强': ['12652141777', '材料'], '庚同硕': ['14388240417', '自动化'], '王岩': ['11277291473', '文法']}
+print (dic)
+menu()
+n=input()
+if n=='1':
+    name=input()
+    if name  not in dic.keys():
+        number=input()
+        major=input()
+        lt=[number,major]
+        dic[name]=lt
+        print('Success')
     else:
-        s=letter.index(i)
-        new_string+=new_letter[s]
-print (new_string)
+        print('Fail')
+    print (dic)
+else:
+    print('ERROR')
